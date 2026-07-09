8aeb4e48118429baebe2906619b4a34fd34467e5
Saivinu1425 <ssaivinu@gmail.com>
2026-07-09 20:29:45 +0530

    Add app radius design system

---
+++ lib/core/design_system/app_radius.dart
@@ -0,0 +1,39 @@
+class AppRadius {
+  AppRadius._();
+
+  /// Extra Small
+  static const double xs = 4.0;
+
+  /// Small
+  static const double sm = 8.0;
+
+  /// Medium
+  static const double md = 12.0;
+
+  /// Large
+  static const double lg = 16.0;
+
+  /// Extra Large
+  static const double xl = 20.0;
+
+  /// 2X Large
+  static const double xxl = 24.0;
+
+  /// Rounded Cards
+  static const double card = 18.0;
+
+  /// Buttons
+  static const double button = 14.0;
+
+  /// TextFields
+  static const double input = 14.0;
+
+  /// Dialogs
+  static const double dialog = 24.0;
+
+  /// Bottom Sheets
+  static const double bottomSheet = 28.0;
+
+  /// Circular Avatar
+  static const double avatar = 999.0;
+}
